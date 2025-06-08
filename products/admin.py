6be97757e343aff992da1860175cbbfdd595549f
Eleri Hulme <eleri.hulme@live.co.uk>
2025-06-08 19:03:36 +0100

    add categories and shop plants to admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PlantCategory, ShopPlant
+
+class PlantCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'typical_light_requirements', 'typical_watering_frequency', 'typical_caring_difficulty')
+
+class ShopPlantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'environment', 'pet_friendly', 'air_purifying')
+    search_fields = ('name', 'botanical_name', 'description')
+    list_filter = ('category', 'environment', 'pet_friendly', 'air_purifying')
+
+admin.site.register(PlantCategory, PlantCategoryAdmin)
+admin.site.register(ShopPlant, ShopPlantAdmin)
