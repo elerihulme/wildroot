b7618297b568149de46a10e60849bfe76bbc985c
Eleri Hulme <eleri.hulme@live.co.uk>
2025-06-08 15:11:44 +0100

    prepare for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn wildroot.wsgi
--- wildroot/settings.py
+++ wildroot/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ydubce^(kgsox!yy7f#9l!-wnfn9+a9@g1qi2kj%=gb8r94ta)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
