385e8eeb7f61e1e035167f3ded86f6c23dee921d
Eleri Hulme <eleri.hulme@live.co.uk>
2025-06-09 16:28:48 +0100

    add adjust quantity functionality to bag

--- wildroot/urls.py
+++ wildroot/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
-    path('checkout/', include('checkout.urls')),
     path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
