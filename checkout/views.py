4c01b2cf45e1d6a7a6437dab8ff013f9203284ad
Eleri Hulme <eleri.hulme@live.co.uk>
2025-06-13 17:47:53 +0100

    update checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -33,23 +33,15 @@ def cache_checkout_data(request):
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
-    bag = request.session.get('bag', {})
+    
 
-    if not bag:
-        messages.error(request, "There's nothing in your bag at the moment.")
-        return redirect(reverse('products'))
+    
 
-    current_bag = bag_contents(request)
-    total = current_bag['grand_total']
-    stripe_total = round(total * 100)
-
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency='gbp',
-    )
+    
 
     if request.method == 'POST':
+        bag = request.session.get('bag', {})
+
         form_data = {
             'full_name': request.POST['full_name'],
             'email': request.POST['email'],
@@ -82,6 +74,20 @@ def checkout(request):
         else:
             messages.error(request, 'There was an error with your form. Please check your information.')
     else:
+        bag = request.session.get('bag', {})
+        if not bag:
+            messages.error(request, "There's nothing in your bag at the moment.")
+            return redirect(reverse('products'))
+        
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        stripe_total = round(total * 100)
+
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency='gbp',
+        )
         order_form = OrderForm()
 
     context = {
